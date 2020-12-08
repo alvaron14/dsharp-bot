30db80298d21f0e2ae91beef176f73debb736a01
Álvaro Gómez Cuenca <basura1200@gmail.com>
2020-12-08 12:31:24 +0100

    Hotfix for heroku deployment

--- Bot_Discord_CSharp/Program.cs
+++ Bot_Discord_CSharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Cqrs.Hosts;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
 
 namespace Bot_Discord_CSharp
 {
@@ -6,8 +9,18 @@ namespace Bot_Discord_CSharp
     {
         static void Main(string[] args)
         {
+            var host = CreateWebHostBuilder(args).
+               UseKestrel().
+               UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("$PORT")).
+               Build();
+
+            host.Run();
+
             Bot bot = new Bot();
             bot.RunAsync().GetAwaiter().GetResult();
         }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<StartUp>();
     }
 }

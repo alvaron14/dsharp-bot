3feb5243e0a9ae85db437199c6b374bb0d089e28
Álvaro Gómez Cuenca <basura1200@gmail.com>
2020-12-08 20:42:25 +0100

    Delete port

--- Bot_Discord_CSharp/Program.cs
+++ Bot_Discord_CSharp/Program.cs
@@ -1,6 +1,4 @@
-﻿using ikvm.runtime;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using System;
 using System.IO;
 
@@ -10,16 +8,6 @@ namespace Bot_Discord_CSharp
     {
         static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .UseIISIntegration()
-            .UseStartup<Startup>()
-            .UseUrls("http://0.0.0.0:" + Environment.GetEnvironmentVariable("$PORT"))
-            .Build();
-
-            host.Run();
-
             Bot bot = new Bot();
             bot.RunAsync().GetAwaiter().GetResult();
         }
